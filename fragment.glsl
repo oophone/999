0a581a2a2d407f964262933b9de63e8d1b525530
oophone <68236132+oophone@users.noreply.github.com>
2021-06-03 18:08:26 +0800

    Add files via upload

--- fragment.glsl
+++ fragment.glsl
@@ -1,25 +1,55 @@
-#ifdef GL_ES
+#ifdef GL_ES 
 precision mediump float;
 #endif
 
-uniform vec3 u_LightPos;
-
+// sampler for texture number 0 (ship texture)
+uniform sampler2D u_texture;
+uniform samplerCube sCubemapTexture;
+// texture coordnate of texture 0 (set in vertex shader)
+varying vec2 v_texCoord0;
+ const float Eta = 0.15; // Water
+ 
+varying vec3 incident;
+uniform float iTime;
 varying vec3 v_Position;
 varying vec4 v_Color;
 varying vec3 v_Normal;
-
-void main()
-{
-    // compute and normalize light vector
+void main() {
+     vec3 u_LightPos=vec3(-50.0, -3.0, -50.0);
+     // compute and normalize light vector
     float distance = length(u_LightPos - v_Position);
     vec3 lightVector = normalize(u_LightPos - v_Position);
 
     // compute dot product between normal vector and light vector
     // consider only values 0.1 or higher so dark fragments are not totally black
     float diffuse = max(dot(v_Normal, lightVector), 0.1);
+    vec3 r=reflect(v_Normal, lightVector);
+    vec3 v=lightVector;
+    float S = pow( max(dot(r,v),0.1), 2.0);
 
+
+    vec3 v_refraction=refract(incident, v_Normal,Eta);
+    vec3  v_reflection = reflect(incident, v_Normal);
+    float fresnel = Eta + (1.0 - Eta) * pow(max(0.0, 1.0 - dot(-incident,v_Normal)), 5.0);
+	
+    
     // make more distant points darker
     diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));
-
-    gl_FragColor = v_Color * diffuse;
+    S = S * (1.0 / (1.0 + (0.25 * distance * distance)));
+    
+    
+			
+    vec4 diff = v_Color * diffuse;
+    vec4 spec = v_Color * S;
+     vec4 refractionColor = textureCube(sCubemapTexture, normalize(v_refraction))
+     +texture2D(u_texture,v_texCoord0);
+    
+    vec4 reflectionColor = textureCube(sCubemapTexture, normalize(v_reflection))
+    +texture2D(u_texture,v_texCoord0);
+    
+	vec4 col=mix(refractionColor,reflectionColor,fresnel);
+    // bind texture "pixel" to one fragment using texture2D function
+    col.a=.5;
+    gl_FragColor =col+spec+diff;
+    
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,12 +1,26 @@
 // a_position is a default name for position attribute (set programatically as ShaderProgram.POSITION_ATTRIBUTE in java code)
 attribute vec3 a_position;
-// a_color is a default name for color attribute (set programatically as ShaderProgram.COLOR_ATTRIBUTE in java code)
+// coordinates of texture 0 "pixel"
+attribute vec2 a_texCoord0;
+
+// projection view transformation matrix (set programatically in java code)
+uniform mat4 u_projViewTrans;
+
+// world transformation matrix (set programatically in java code)
+uniform mat4 u_worldTrans;
+// move matrix
+uniform mat4 u_moveMatrix;
+
+// texture coordinate passed to vertex shader (set in main())
+varying vec2 v_texCoord0;
+ 
+ uniform sampler2D normalTexture;
+ 
+ // a_color is a default name for color attribute (set programatically as ShaderProgram.COLOR_ATTRIBUTE in java code)
 attribute vec3 a_color;
 // a_normal is a default name for normal vector attribute (set programatically as ShaderProgram.NORMAL_ATTRIBUTE in java code)
 attribute vec3 a_normal;
 
-// rotation matrix (set programatically in java code)
-uniform mat4 u_rotationMatrix;
 
 // vertex position passed to fragment shader (set in main())
 varying vec3 v_Position;
@@ -14,15 +28,55 @@ varying vec3 v_Position;
 varying vec4 v_Color;
 // vertex color passed to fragment shader (set in main())
 varying vec3 v_Normal;
+const float PI = 3.1415926535897932384626433832795;
+
+const float waveLength = 4.0;
+const float waveAmplitude = 0.2;
+uniform float iTime;
+varying vec3 incident;
+float generateOffset(float x, float z, float val1, float val2){
+	float radiansX = ((mod(x+z*x*val1, waveLength)/waveLength) + iTime * mod(x * 0.8 + z, 1.5)) * 2.0 * PI;
+	float radiansZ = ((mod(val2 * (z*x +x*z), waveLength)/waveLength) + iTime * 2.0 * mod(x , 2.0) ) * 2.0 * PI;
+	return waveAmplitude * 0.5 * (sin(radiansZ) + cos(radiansX));
+}
+
+vec3 applyDistortion(vec3 vertex){
+	float xDistortion = generateOffset(vertex.x, vertex.z, 0.2, 0.1);
+	float yDistortion = generateOffset(vertex.x, vertex.z, 0.1, 0.3);
+	float zDistortion = generateOffset(vertex.x, vertex.z, 0.15, 0.2);
+	return vertex + vec3(xDistortion, yDistortion, zDistortion);
+}
+
 
 
 void main() {
-    // compute vertex position on screen (rotate it using rotation maxtrix)
-    v_Position = vec3(u_rotationMatrix * vec4(a_position, 0.0));
-    // compute normal position on screen (rotate it using rotation maxtrix)
-    v_Normal = vec3(u_rotationMatrix * vec4(a_normal, 0.0));
-    // reassign color to v_Color so it can be passed to fragment shader
-    v_Color = vec4(a_color, 0.0);
+    // pass texture coordinate to fragment shader
+    v_texCoord0 = a_texCoord0;
+    
+    vec3 currentVertex = vec3(a_position);
+	currentVertex = applyDistortion(currentVertex);
+   
+   
+   
+    vec4 vertex = u_worldTrans*vec4(currentVertex, 0.0);
+	vec4 u_camera=u_projViewTrans*vec4(currentVertex, 1.0);
+	incident = normalize(vec3(u_camera));
 
-    gl_Position = u_rotationMatrix * vec4(a_position, 1.0);
+    
+  // compute vertex position on screen (rotate it using rotation maxtrix)
+    v_Position = -vec3(u_projViewTrans * vec4(currentVertex, 0.0));
+    // compute normal position on screen (rotate it using rotation maxtrix)
+    v_Normal = vec3(u_projViewTrans * vec4(a_normal, 0.0));
+    // reassign color to v_Color so it can be passed to fragment shader
+    v_Color = vec4(.4,.4,.4, .4);
+   
+  /*  vec3 textNormal = normalize(texture2D(normalTexture, a_texCoord0).rgb*2.0 - 1.0);
+
+    vec3 worldNormal = vec3(mat3(u_projViewTrans[0].xyz, u_projViewTrans[1].xyz, u_projViewTrans[2].xyz) * (a_normal+textNormal));
+
+     v_Normal = worldNormal;*/
+	
+    
+    // compute vertex position on screen
+    gl_Position = u_projViewTrans * u_worldTrans * u_moveMatrix * vec4(currentVertex, 1.0);
 }
